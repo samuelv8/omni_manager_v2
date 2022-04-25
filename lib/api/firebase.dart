0432b1cebe9ac0d3594504eea5f20e8fd7c13149
skinnymega <77574684+skinnymega@users.noreply.github.com>
2022-04-24 23:45:04 -0300

    [OMN-13] sending email to users when forms are released

--- lib/api/firebase.dart
+++ lib/api/firebase.dart
@@ -1,6 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:omni_manager/api/auth.dart';
+import 'package:sendgrid_mailer/sendgrid_mailer.dart';
 
 final FirebaseFirestore _firestore = FirebaseFirestore.instance;
 final CollectionReference _users = _firestore.collection('Users');
@@ -33,14 +34,15 @@ class Database {
       return true;
     });
   }
-  
+
   static Future<bool> checkEmailValidated() async {
     // flow: vai verificar se email já foi verificado, caso contrário envia email de verificaçao novamente
     User? user = FirebaseAuth.instance.currentUser;
-    if (user!= null && !user.emailVerified) {
+    if (user != null && !user.emailVerified) {
       await user.sendEmailVerification();
     }
-    if (user!= null) return user.emailVerified;
+    if (user != null)
+      return user.emailVerified;
     else {
       print("Current user not found.");
       return false;
@@ -136,6 +138,9 @@ class Database {
                 await element.get(FieldPath(['ref'])).get();
             await Future.wait(
                 [addForm(user.id, false), addForm(user.id, true)]);
+            print(user);
+            final email = await element.get(['email']).get();
+            await Future.wait([sendEmail(email)]);
           })
         });
   }
@@ -172,4 +177,33 @@ class Database {
   static Future<bool> emailExistsInDatabase(String userEmail) {
     return getUserFromEmail(userEmail).then((value) => value.docs.isNotEmpty);
   }
+}
+
+Future sendEmail(String userEmail) async {
+  var body = '''
+
+    Olá!
+
+    Você recebeu seu formulário semanal na plataforma OmniManager.
+
+    Não vai esquecer de preencher, ein?
+
+    Att.,
+
+    Equipe OmniManager
+
+    ''';
+  final mailer = Mailer(
+      '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');
+  final toAddress = Address(userEmail);
+  final fromAddress = Address('Alexandre.bernat@ga.ita.br');
+  final content = Content('text/plain', body);
+  final subject = 'Atenção para formulário de acompanhamento recebido!';
+  final personalization = Personalization([toAddress]);
+
+  final email =
+      Email([personalization], fromAddress, subject, content: [content]);
+  return mailer.send(email).then((result) {
+    print(result);
+  });
 }
